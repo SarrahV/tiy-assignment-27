f426a287e863e55de33a2d66ff21da62d64cb74f
Sarrah Vesselov <sarchka1@gmail.com>
2015-03-18 17:02:18 -0400

    removed extra crap

--- js/main.js
+++ js/main.js
@@ -7,15 +7,9 @@ $(function(){
     issues.each(function(model){
 
       var issueView = new IssueView({model: model});
-
       $(".backbone-view").append(issueView.render().el);
+      
     });
-
   });
-
 });
-  // var issueView = new IssueView({model: issues.first()});
-  // console.log(issues.toJSON());
-  // $(".backbone-view").append(issueView.render().el);
 
-  // });
